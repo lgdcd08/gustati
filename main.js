8700dd893d3a0b3b0d29cddda25c7f3dd056f214
Luiz Gustavo de cARVALHO DAGNESE <lgdcd08@gmail.com>
2025-02-24 17:16:48 -0300

    assistpionner

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 console.log("Processo principal")
 
-const { app, BrowserWindow, nativeTheme } = require('electron')
+const { app, BrowserWindow, nativeTheme, Menu } = require('electron')
 
-//Janela principal let win
+// Janela principal
 let win
-
-//const é uma função , new é um objeto que acessa alt e larg, autohide é para mexer no estilo, minimizable false(nao deixa minimizar a janela), resizable tira o maximiar e o minimizar além de bloquear o tamanho da tela
 const createWindow = () => {
-    //a linha abaixo define o tem claro ou escuro
-    nativeTheme.themeSource = 'light' //dark ou light, system se adpata a cor do sistema
-    const win = new BrowserWindow({
+    // a linha abaixo define o tema (claro ou escuro)
+    nativeTheme.themeSource = 'light' //(dark ou light)
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         //autoHideMenuBar: true,
@@ -17,9 +15,36 @@ const createWindow = () => {
         resizable: false
     })
 
+    // menu personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
     win.loadFile('./src/views/index.html')
 }
-//iniciar a aplicação, darwin é para mac
+
+// Janela sobre
+function aboutWindow() {
+    nativeTheme.themeSource = 'light'
+    // a linha abaixo obtém a janela principal
+    const main = BrowserWindow.getFocusedWindow()
+    let about
+    // Estabelecer uma relação hierárquica entre janelas
+    if (main) {
+        // Criar a janela sobre
+        about = new BrowserWindow({
+            width: 360,
+            height: 220,
+            autoHideMenuBar: true,
+            resizable: false,
+            minimizable: false,
+            parent: main,
+            modal: true
+        })
+    }
+    //carregar o documento html na janela
+    about.loadFile('./src/views/sobre.html')
+}
+
+// Iniciar a aplicação
 app.whenReady().then(() => {
     createWindow()
 
@@ -35,3 +60,80 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+//reduzir logs não críticos
+app.commandLine.appendSwitch('log-level', '3')
+
+// template do menu
+const template = [
+    {
+        label: 'Cadastro',
+        submenu: [
+            {
+                label: 'Clientes'
+            },
+            {
+                label: 'OS'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Sair',
+                click: () => app.quit(),
+                accelerator: 'Alt+F4'
+            }
+        ]
+    },
+    {
+        label: 'Relatórios',
+        submenu: [
+            {
+                label: 'Clientes'
+            },
+            {
+                label: 'OS abertas'
+            },
+            {
+                label: 'OS concluídas'
+            }
+        ]
+    },
+    {
+        label: 'Ferramentas',
+        submenu: [
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrão',
+                role: 'resetZoom'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Recarregar',
+                role: 'reload'
+            },
+            {
+                label: 'Ferramentas do desenvolvedor',
+                role: 'toggleDevTools'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Sobre',
+                click: () => aboutWindow()
+            }
+        ]
+    }
+]
---
+++ src/public/js/script.js
@@ -0,0 +1,13 @@
+function obterData() {
+    const dataAtual = new Date()
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return dataAtual.toLocaleDateString('pt-BR', options)
+}
+
+
+document.getElementById('dataAtual').innerHTML = obterData()
